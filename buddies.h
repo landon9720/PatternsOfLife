9aacbe2e5e7987fcd0fd0aa34f194143d3d46eff
Landon Kuhn <landon9720@gmail.com>
2015-03-20 21:43:39 -0700

    checkpoint; more or less complete port from box2d to hex

---
+++ buddies.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cmath>
+#include <random>
+#include <cassert>
+#include <set>
+#include <fann.h>
+#ifdef __EMSCRIPTEN__
+#include "easygame_emscripten.h"
+#include <emscripten.h>
+#else
+#include "easygame.h"
+#endif
+
+#include <OpenGL/gl.h>
+
+using std::min;
+using std::max;
